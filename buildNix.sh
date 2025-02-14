98c38b3e836667a2b3540d54660a29a362a50556
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2025-02-14 09:42:01 -0500

    Fixes #11 Use separate nuspec files for different architechtures

--- buildNix.sh
+++ buildNix.sh
@@ -27,13 +27,20 @@ else
     Platform=Ubuntu22
 fi
 
+if [ "$(uname -m)" == "arm64" ]; 
+then
+    Arch=ARM64
+else
+    Arch=x64
+fi
+
 for BuildType in Debug Release
 do
-    cmake -BBuildCVODES_${Platform}/${BuildType}/x64/ -Hsrc/CVODES/ -DCMAKE_BUILD_TYPE=${BuildType} -DEXAMPLES_ENABLE_C=OFF -DBUILD_SHARED_LIBS=OFF -DBUILD_STATIC_LIBS=ON -DENABLE_KLU=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DKLU_INCLUDE_DIR=BuildSuiteSparse/include/ -DKLU_LIBRARY_DIR=BuildSuiteSparse/lib64/
-    make -C BuildCVODES_${Platform}/${BuildType}/x64/
+    cmake -BBuildCVODES_${Platform}/${BuildType}/${Arch}/ -Hsrc/CVODES/ -DCMAKE_BUILD_TYPE=${BuildType} -DEXAMPLES_ENABLE_C=OFF -DBUILD_SHARED_LIBS=OFF -DBUILD_STATIC_LIBS=ON -DENABLE_KLU=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DKLU_INCLUDE_DIR=BuildSuiteSparse/include/ -DKLU_LIBRARY_DIR=BuildSuiteSparse/lib64/
+    make -C BuildCVODES_${Platform}/${BuildType}/${Arch}/
     
-    mkdir -p Dist/${Platform}/${BuildType}/x64
-    find ./BuildCVODES_${Platform} -name lib*.a -exec cp {} Dist/${Platform}/${BuildType}/x64/ \;
+    mkdir -p Dist/${Platform}/${BuildType}/${Arch}
+    find ./BuildCVODES_${Platform} -name lib*.a -exec cp {} Dist/${Platform}/${BuildType}/${Arch}/ \;
 done
 
 
